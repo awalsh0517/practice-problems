a969089b0d0706ddf4eed0a7bad5d1b289a5f37d
Andrea Walsh <andreawalsh@Andreas-MacBook-Air.local>
2021-01-17 10:19:29 -0500

    for loop version

--- lucky-seven.js
+++ lucky-seven.js
@@ -1,3 +1,24 @@
-const luckySeven = () => {
-  return
+// Loop through adding 3 consecutive numbers together to try and reach 7
+// Feel like it can do it with a reduce.
+
+// const arr = [1, 2, 4, 3]
+
+// const luckySeven = (arr) => {
+//   return arr.reduce((acc, el) => {
+//     return
+//   }, true)
+// }
+
+
+// for loop way
+
+const arr = [1, 2, 4, 3]
+
+const luckySeven = (arr) => {
+  for (let i = 2; i < arr.length; i++) {
+    if (arr[i] + arr[i - 1] + arr[i - 2] === 7) {
+      return true
+    }
+  }
+  return false
 }
